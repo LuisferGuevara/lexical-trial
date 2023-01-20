68369ed7b327a79555277344f320722ae2990787
Luisfer Guevara <luisferguevaraa@gmail.com>
2023-01-20 16:54:34 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import Editor from './Components/Editor';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Plain Text Example</h1>
+      <p>Note: this is an experimental build of Lexical</p>
+      <Editor/>
+
     </div>
   );
 }
